1f224a7d79c1abd102bd08706d70a1300f48ebb0
crobledo2 <crobledo@soaint.com>
2021-03-23 17:29:27 -0500

    no message

---
+++ pruebaOpenshift/src/main/java/com/example/pruebaOpenshift/PruebaOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebaOpenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaOpenshiftApplication.class, args);
+	}
+
+}
---
+++ pruebaOpenshift/src/test/java/com/example/pruebaOpenshift/PruebaOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebaOpenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
