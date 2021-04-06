4d01f98c914d450cc49f0af6c0384a3153ad760a
crobledo2 <crobledo@soaint.com>
2021-04-06 16:17:09 -0500

    no message

---
+++ pomprube/src/test/java/com/example/pomprube/PomprubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pomprube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomprubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
